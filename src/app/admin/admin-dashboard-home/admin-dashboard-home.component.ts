d62099b351bb5c64503962eb916093e53077f020
George Kwabena Asiedu <90930379+george-asiedu@users.noreply.github.com>
2024-07-03 17:43:14 +0000

    admin home dashboard component updated

---
+++ src/app/admin/admin-dashboard-home/admin-dashboard-home.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-dashboard-home',
+  standalone: true,
+  imports: [],
+  templateUrl: './admin-dashboard-home.component.html',
+  styleUrl: './admin-dashboard-home.component.scss'
+})
+export class AdminDashboardHomeComponent {
+
+}
