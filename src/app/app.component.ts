55efef94b075e6b5b508171a55e8316bcfeddc0f
George Kwabena Asiedu <90930379+george-asiedu@users.noreply.github.com>
2024-07-03 17:45:52 +0000

    component updated to use ngToastModule

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { NgToastModule } from 'ng-angular-popup';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, NgToastModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,18 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async'
+import { provideHttpClient } from '@angular/common/http';
+import { provideSpinnerConfig } from "ngx-spinner";
+import { NgToastModule } from 'ng-angular-popup';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
-};
+  providers: [
+    provideRouter(routes), 
+    provideAnimationsAsync(), 
+    provideHttpClient(), 
+    provideSpinnerConfig({ type: 'ball-clip-rotate-multiple'}),
+    importProvidersFrom(NgToastModule)
+  ]
+}
