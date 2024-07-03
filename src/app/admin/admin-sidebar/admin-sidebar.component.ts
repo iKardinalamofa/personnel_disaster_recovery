c76a2e407dba7ea19b48d213e96bbc9f281316d2
George Kwabena Asiedu <90930379+george-asiedu@users.noreply.github.com>
2024-07-03 17:44:18 +0000

    admin sidebar component created

---
+++ src/app/admin/admin-sidebar/admin-sidebar.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-admin-sidebar',
+  standalone: true,
+  imports: [
+    RouterModule
+  ],
+  templateUrl: './admin-sidebar.component.html',
+  styleUrl: './admin-sidebar.component.scss'
+})
+export class AdminSidebarComponent {
+
+}
