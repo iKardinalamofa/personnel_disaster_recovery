bcf1af3868422c6d8a5bb313c7244cc7c97fb6a3
George Kwabena Asiedu <90930379+george-asiedu@users.noreply.github.com>
2024-07-03 17:48:18 +0000

    verify email component created

---
+++ src/app/auth/verify-email/verify-email.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-verify-email',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './verify-email.component.html',
+  styleUrl: './verify-email.component.scss'
+})
+export class VerifyEmailComponent {
+
+}
